2bc2c5e7c82c271eb0666f58d9b7fdfe33ec23e9
Emirhan Yolcu <103894115+passecy@users.noreply.github.com>
2022-07-03 21:31:55 +0300

    Add files via upload

---
+++ scratch_3.java
@@ -0,0 +1,48 @@
+
+import java.util.Objects;
+import java.util.Scanner;
+class Scratch {
+    public static void main(String[] args) {
+        int k,x,y,average;
+        x=0;
+        y=0;
+
+
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("Number:");
+        k=input.nextInt();
+
+        for (int i = 1; i <k ; i++) {
+            if ((i % 3 == 0) && (i % 4 == 0)) {
+                x = x + i;
+                y = y + 1;
+
+
+            }
+        }
+
+        if ( y==0 ) {
+            System.out.println("None");
+        }
+        else{
+            System.out.println(x/y);
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
