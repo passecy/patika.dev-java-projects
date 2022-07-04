12fe2d39ec06acc6cf8902789aef89e541b43031
Emirhan Yolcu <103894115+passecy@users.noreply.github.com>
2022-07-04 19:33:22 +0300

    Update and rename Main.java to ATM.java

--- ATM.java
+++ ATM.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class ATM {
     public static void main(String[] args) {
         String userName, password;
         Scanner input = new Scanner(System.in);
